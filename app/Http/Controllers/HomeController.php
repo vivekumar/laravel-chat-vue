34527f8c94736095d14054a4558e8d3f0e23a481
vivek kumar <vivekbcagcg@gmail.com>
2022-09-11 02:21:51 +0530

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Message;
+use Auth;
+use App\Events\SendMessage;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+    public function chat()
+    {
+        return view('chat');
+    }
+    public function message()
+    {
+        return Message::with('user')->get();
+    }
+    public function messageStore(Request $request)
+    {
+        //return $request;
+        $user = Auth::user();
+        $messages=$user->messages()->create([
+            'message'=>$request->message,
+        ]);
+        broadcast(new SendMessage($user,$messages))->toOthers();
+        return 'message sends';
+    }
+}
